8c45486379ab481436114573681dcd2c76480101
Sundayfree <978591397@qq.com>
2019-07-30 09:50:05 +1200

    auth page done

---
+++ src/screens/AuthLoading.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Container, Header, Content, Spinner, Body } from 'native-base';
+import AsyncStorage from '@react-native-community/async-storage';
+
+export class AuthLoading extends Component {
+  constructor(props) {
+    super(props);
+    this._bootstrapAsync();
+  }
+
+  _bootstrapAsync = async () => {
+    const userToken = await AsyncStorage.getItem('token');
+    console.log(userToken);
+
+    this.props.navigation.navigate(userToken ? 'home' : 'login');
+  };
+  render() {
+    return (
+      <Container
+        style={{
+          backgroundColor: '#1f3954',
+          justifyContent: 'center'
+        }}
+      >
+        <Spinner color="#fff" />
+      </Container>
+    );
+  }
+}
+
+export default AuthLoading;
