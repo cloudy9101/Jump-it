57fceb8dd8970589f9a3557515a1572883731c31
Sundayfree <978591397@qq.com>
2019-07-30 09:56:35 +1200

    auth done

--- src/screens/LogoutPage.js
+++ src/screens/LogoutPage.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import { Container, Button } from 'native-base';
+import AsyncStorage from '@react-native-community/async-storage';
 import HeaderComponent from '../components/DrawerHeaderComponent';
 export default class LogoutPage extends Component {
+  constructor(props) {
+    super(props);
+    this.logoutHandler = this.logoutHandler.bind(this);
+  }
+  async logoutHandler() {
+    await AsyncStorage.removeItem('token');
+    this.props.navigation.navigate('SignIn');
+  }
   render() {
     return (
       <Container style={{ backgroundColor: '#1f3954' }}>
@@ -12,7 +21,7 @@ export default class LogoutPage extends Component {
           rounded
           danger
           style={{ marginTop: 30, marginLeft: 10, marginRight: 10 }}
-          onPress={() => this.props.navigation.navigate('SignIn')}
+          onPress={this.logoutHandler}
         >
           <Text>Logout</Text>
         </Button>
