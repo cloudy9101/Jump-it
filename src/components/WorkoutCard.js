0f1815e58b1f9d8d9a3d4f8734cd0cfffef9992e
Sundayfree <978591397@qq.com>
2019-07-30 22:22:08 +1200

    step display done

--- src/components/WorkoutCard.js
+++ src/components/WorkoutCard.js
@@ -15,59 +15,59 @@ export default function WorkoutCard(props) {
         shadowOpacity: 0.7
       }}
     >
-      <TouchableOpacity>
-        <View
-          style={{
-            height: 110,
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-between',
-            alignItems: 'center'
-          }}
-        >
-          <View style={{ width: 180 }}>
-            <Text
-              style={{
-                color: '#ffffff',
-                fontFamily: 'Helvetica',
-                fontSize: 20,
-                paddingLeft: 6
-              }}
-            >
-              {props.name}
-            </Text>
-          </View>
-          <View style={{ flexDirection: 'row', alignItems: 'baseline' }}>
-            <Text
-              style={{ color: '#ffffff', fontFamily: 'Courier', fontSize: 36 }}
-            >
-              {props.num}
-            </Text>
-            <Text
-              style={{
-                color: '#ffffff',
-                fontFamily: 'Helvetica',
-                fontSize: 16,
-                paddingRight: 8
-              }}
-            >
-              {props.unit}
-            </Text>
-          </View>
-        </View>
-        <View style={{ flexDirection: 'row-reverse' }}>
+      {/* <TouchableOpacity> */}
+      <View
+        style={{
+          height: 110,
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center'
+        }}
+      >
+        <View style={{ width: 180 }}>
           <Text
             style={{
               color: '#ffffff',
               fontFamily: 'Helvetica',
-              fontSize: 14,
+              fontSize: 20,
+              paddingLeft: 6
+            }}
+          >
+            {props.name}
+          </Text>
+        </View>
+        <View style={{ flexDirection: 'row', alignItems: 'baseline' }}>
+          <Text
+            style={{ color: '#ffffff', fontFamily: 'Courier', fontSize: 36 }}
+          >
+            {props.num}
+          </Text>
+          <Text
+            style={{
+              color: '#ffffff',
+              fontFamily: 'Helvetica',
+              fontSize: 16,
               paddingRight: 8
             }}
           >
-            Today: at {props.time}
+            {props.unit}
           </Text>
         </View>
-      </TouchableOpacity>
+      </View>
+      <View style={{ flexDirection: 'row-reverse' }}>
+        <Text
+          style={{
+            color: '#ffffff',
+            fontFamily: 'Helvetica',
+            fontSize: 14,
+            paddingRight: 8
+          }}
+        >
+          Today: at {props.time}
+        </Text>
+      </View>
+      {/* </TouchableOpacity> */}
     </View>
   );
 }
