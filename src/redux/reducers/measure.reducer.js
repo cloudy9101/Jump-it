b240ef00bf2b7477d462e06b3055faf88323f86c
Sundayfree <978591397@qq.com>
2019-08-16 20:58:27 +1200

    bug of week display fixed

--- src/redux/reducers/measure.reducer.js
+++ src/redux/reducers/measure.reducer.js
@@ -9,40 +9,23 @@ const initState = {
 export const highblood = (state = initState, action) => {
   switch (action.type) {
     case GET_HIGHBLOOD_DATA: {
-      const { datasets, labels, startIndex } = action.payload;
-      const data = getData(datasets, startIndex);
-      return { ...initState, datasets: data, labels, isLoading: true };
-    }
-    default:
-      return state;
-  }
-};
-// const sugarState = {
-//   labels: [],
-//   datasets: [],
-//   isLoading: false
-// };
-export const sugar = (state = initState, action) => {
-  switch (action.type) {
-    case GET_SUGAR_DATA: {
-      const { datasets, labels, startIndex } = action.payload;
-      const data = getData(datasets, startIndex);
-      return { ...initState, datasets: data, labels, isLoading: true };
+      const { datasets, labels } = action.payload;
+
+      return { ...initState, datasets, labels, isLoading: true };
     }
     default:
       return state;
   }
 };
 
-function getData(arr, startIndex) {
-  arr.forEach(e => {
-    if (e.data.length !== 0) {
-      for (let i = 0; i < startIndex; i++) {
-        e.data.unshift(0);
-      }
-    } else {
-      e.data.push(0);
+export const sugar = (state = initState, action) => {
+  switch (action.type) {
+    case GET_SUGAR_DATA: {
+      const { datasets, labels } = action.payload;
+
+      return { ...initState, datasets, labels, isLoading: true };
     }
-  });
-  return arr;
-}
+    default:
+      return state;
+  }
+};
